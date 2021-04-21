8dae77212f128c3c8be36ce3a350501b00b2b27c
maxamo203 <maxamo.203@gmail.com>
2021-04-21 16:53:49 -0300

    pequenios cambios

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ var cantidadEntrdas = 1;
 function calcularTiempo(){
     'use strict'
     let long = document.getElementsByClassName('velocidad').length;
-    
+    var bien = true;
     var tiempo = 0;
     for(var i = 0;i < long; i++){
         
@@ -13,8 +13,12 @@ function calcularTiempo(){
         var cant_pentagramas = parseFloat(document.getElementsByClassName('c_pentagramas')[i].value);
         var cant_compases_repetidos =parseFloat(document.getElementsByClassName('c_compases_repetidos')[i].value);
         var cant_compases_extras = parseFloat(document.getElementsByClassName('c_compases_extras')[i].value);
-        if (velocidad > 1 || cant_compases > 1 )/*etc*/{
-            
+        if (velocidad > 0 && cant_compases > 0 && cant_tiempos > 0 && cant_pentagramas > 0 && bien)/*etc*/{
+            document.getElementsByClassName("velocidad")[0].style.backgroundColor = 'rgb(192, 201, 229)';
+            document.getElementsByClassName("c_compases")[0].style.backgroundColor = 'rgb(192, 201, 229)';
+            document.getElementsByClassName("c_tiempos")[0].style.backgroundColor = 'rgb(192, 201, 229)';
+            document.getElementsByClassName("c_pentagramas")[0].style.backgroundColor = 'rgb(192, 201, 229)';
+
             velocidad = 60/velocidad;
             cant_compases = cant_compases * cant_pentagramas;
             cant_compases += cant_compases_repetidos + cant_compases_extras;
@@ -24,16 +28,27 @@ function calcularTiempo(){
                     tiempo += velocidad;
                 }
             }
-            document.querySelector("#salida").innerHTML = "La partitura dura " + sexagecimal_lenguaje(decimal_sexagecimal(tiempo));
+            document.querySelector("#salida").innerHTML = "La partitura dura " + sexagecimal_lenguaje(decimal_sexagecimal(tiempo.toFixed(1)));
+            document.querySelector("#salida").style.color = "#FFFFFF"
         }
         else{
-            alert("Deben ser mayor que 0");
+            bien = false;
+            //if (velocidad <= 0){a = document.getElementsByClassName("velocidad")[i];a.style.backgroundColor = '#FF0000';}
+            //if (cant_compases <= 0) document.getElementsByClassName("c_compases")[i].style.backgroundColor = '#FF0000';
+            //if (cant_tiempos <= 0) document.getElementsByClassName("c_tiempos")[i].style.backgroundColor = '#FF0000';
+            //if (cant_pentagramas < 1) document.getElementsByClassName("c_pentagramas")[i].style.backgroundColor = '#FF0000';
+            //alert("Todos los datos tienen que tener un numero");
+            
+            var a = document.querySelector("#salida");
+            a.innerHTML = "No fue posible calcular el tiempo, faltan datos."
+            a.style.color = '#FF0000';
+            return;
         } 
     }
      
 }
 function decimal_sexagecimal(segs){
-    segs = parseInt(segs);
+    segs = parseFloat(segs);
     horas = 0; mins = 0;
     while (segs > 59){
         segs -= 60;
@@ -56,10 +71,14 @@ function sexagecimal_lenguaje(sexag){
     sexag = Array.from(sexag)
     var out = "";
     var orden = [["hora","horas"],["minuto","minutos"],["segundo","segundos"]];
+    var long = 3;
    
-    for (var i = 0; i < 3; i++){
+    for (var i = 0; i < long; i++){
         if (sexag[i] != 0){
-            out += sexag[i] +  " " + (sexag[i] > 1 ? orden[i][1]: orden[i][0]) + " "
+            if (i < long - 1)
+                out += sexag[i] +  " " + (sexag[i] > 1 ? orden[i][1]: orden[i][0]) + " ";
+            else
+                out += sexag[i].toFixed(2) +  " " + (sexag[i] > 1 ? orden[i][1]: orden[i][0]) + " ";
         }
     }
     return out
